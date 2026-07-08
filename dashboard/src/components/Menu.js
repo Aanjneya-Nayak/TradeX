8df9625786b339938465753cc4a86fce58ad25f7
Aanjneya Nayak <aanjneya@example.com>
2026-07-08 17:49:23 +0530

    Fix dashboard logo path

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -56,7 +56,7 @@ const Menu = () => {
         className="menu-brand"
         aria-label="Go to landing site"
       >
-        <img src="TradeX.png" alt="TradeX Logo" style={{ width: "50px" }} />
+        <img src="logo.png" alt="Logo" style={{ width: "50px" }} />
       </a>
       <div className="menus">
         <ul>
